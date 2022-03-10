c21d44361ca21d0036c162426a3b9794cfa87782
mcale <mcale@digitalocean.com>
2022-03-10 16:25:57 -0600

    feat: make PORT configurable

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: process.env["PORT"] || 8080,
         host: 'localhost'
     });
 
