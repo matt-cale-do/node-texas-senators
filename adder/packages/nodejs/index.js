451a9c5eca1da06fa88370119707347b55d4c846
mcale <mcale@digitalocean.com>
2022-06-08 15:23:40 -0500

    feat: add contrived error metric

--- adder/packages/nodejs/index.js
+++ adder/packages/nodejs/index.js
@@ -5,6 +5,11 @@ function main(args) {
     const debugData = `The endpoint takes two args when hit with POST you used method: ${args.__ow_method}. The data provided was numOne: ${numOne} and numTwo: ${numTwo}`
 
     const result = (Number.isInteger(numOne) && Number.isInteger(numTwo)) ? numOne + numTwo : -1
+
+    if (result === 4) {
+      throw new Error("your `result` was 4 which this program hates!")
+    }
+
     return { "body": { result, args, debugData } }
   }
   
